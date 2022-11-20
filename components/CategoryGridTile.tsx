142edb561b60b1e08546648a0bf43729ddafba0a
Christian Villamin <ChristianVillamin31@gmail.com>
2022-11-20 17:57:49 +0800

    feat: category grid tile component

---
+++ components/CategoryGridTile.tsx
@@ -0,0 +1,59 @@
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
+
+type Props = {
+  title: string;
+  color: string;
+};
+
+export default function CategoryGridTile({ title, color }: Props) {
+  return (
+    <View style={styles.container}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.pressable,
+          pressed ? styles.pressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    overflow: Platform.select({ android: "hidden", ios: "visible" }),
+    // Android Shadow
+    elevation: 4,
+    // iOS Shadow
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 2 },
+  },
+  pressable: {
+    flex: 1,
+  },
+  pressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
